bf4e71c20b49919c0b8c0c1e540aca9f7d23cd4c
brachalevi <barlevi055@gmail.com>
2023-11-02 12:53:12 +0200

    non

--- client/js/register.js
+++ client/js/register.js
@@ -5,13 +5,7 @@ const register = () => {
         document.getElementById("registerError").innerHTML = "not a valid password"
         return;
     }
-    // const users = getUsers().users;
     const name = document.getElementById("registerName").value;
-    // let user = users.filter(user => user.name === name)[0];
-    // if (user) {
-    //     document.getElementById("registerError").innerHTML = "the name user alredy exist"
-    //     return;
-    // }
 
     const myFAJAX = new FAJAX();
     myFAJAX.onload = function () {
@@ -20,12 +14,7 @@ const register = () => {
         loadPage('contactsPage');
     };
 
-    // Simulate making a request
-    // myFAJAX.open('POST', `users/user/register`, JSON.stringify({name: name, password: password}));
     myFAJAX.open('POST', `users/user/name=${name}+password=${password}`);
     myFAJAX.send();
 
-    // user = addUser(name, password);
-    // localStorage.setItem("currentUser", JSON.stringify(currentUser));
-    // loadPage('contactsPage');
 }
